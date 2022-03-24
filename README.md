70b4b69c830079094c8cf00a457181ee2caae588
simplon-marseille <acardnicolas@hotmail.com>
2022-03-24 16:43:46 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Objectif de l'exercice : Trouver la valeur maximum dans un tableau d'entier.
+# Objectif de l'exercice : Trouver la valeur maximum dans un tableau d'entier avec javascript ES6.
 
 Vous devrez développer votre algorithme sans avoir recours aux fonctions natives de javascript (Math.max(), sort(), etc...)
 Mettez vous dans la peau du développeur qui crée la fonction native permettant de réaliser cette objectif.
@@ -6,24 +6,29 @@ Mettez vous dans la peau du développeur qui crée la fonction native permettant
 Vous pouvez coder à partir du code suivant:
 
 ```
-const numbers = [3, 6, 2, 8, 13, 1]
+const numbers = [3, 23, 7, 8, 10, 11]
 
 const findMaxNumber = (arrayOfNumber) => {
     //Todo...
 }
 
 findMaxNumber(numbers)
-// => 13
+// => 23
 ```
 
 
-# Objectif pédagogique: Appréhender la résolution d'un algorithme en le découpant en somme d'éléments simples.
+# Objectifs pédagogiques: 
+### Appréhender la résolution d'un algorithme en le découpant en somme d'éléments simples.
 
 N'hésitez pas à écrire en langage naturel (français) et **sous forme de commentaires**, les différentes étapes qui vous apparaissent comme logique.
 
 > Ex: 
-> //1 - déclarer une variable
-> //2 - etc...
+- //1 - déclarer une variable
+- //2 - etc...
+
+### Faire intervenir les structures de contrôles et structure conditionnelles de javascript.
+
+
 
 # Exécution du code
 
--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 //1- Déclarons un tableau d'entiers positifs
 const numbers = [3, 23, 7, 8, 10, 11]
 
-//2- J'assigne à une variable la valeur du premier élément du tableau
-//comme étant arbitrairement la plus petite.
-let lePlusGrand = numbers[0]
+const findMaxNumber = (arrayOfNumbers) => {
+    //2- J'assigne à une variable la valeur du premier élément du tableau
+    //comme étant arbitrairement la plus grande.
+    let lePlusGrand = arrayOfNumbers[0]
 
-//3- Celle-ci sera comparée à l'élément suivant dans le cadre de la boucle qui suit
+    //3- Celle-ci sera comparée à l'élément suivant dans le cadre de la boucle qui suit
 
-for (let index = 0; index < numbers.length; index++) {
-    //4- Je compare chaque élément du tableau avec avec le premier élément
-    if (numbers[index] > lePlusGrand){
-        //5- Si la valeur d'un élément du tableau est plus grand qua la valeur contenue dans 'lePlusGrand'
-        //alors on assigne à 'lePlusGrand' sa nouvelle valeur
-        lePlusGrand = numbers[index]
+    for (let index = 0; index < arrayOfNumbers.length; index++) {
+        //4- Je compare chaque élément du tableau avec avec 'lePlusGrand'
+        if (arrayOfNumbers[index] > lePlusGrand) {
+            //5- Si la valeur d'un élément du tableau est plus grand qua la valeur contenue dans 'lePlusGrand'
+            //alors on assigne à 'lePlusGrand' sa nouvelle valeur
+            lePlusGrand = arrayOfNumbers[index]
+        }
     }
+    return lePlusGrand
 }
 
-console.log(lePlusGrand)
+console.log(findMaxNumber(numbers))
